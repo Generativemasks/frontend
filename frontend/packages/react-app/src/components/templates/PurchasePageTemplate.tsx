77969b674e92ac5bfb07bce2c0600bef29362fb4
wild-mouse <wildmouse0813@gmail.com>
2021-07-27 11:28:30 +0900

    UPdate text

--- frontend/packages/react-app/src/components/templates/PurchasePageTemplate.tsx
+++ frontend/packages/react-app/src/components/templates/PurchasePageTemplate.tsx
@@ -416,9 +416,10 @@ const PurchasePageTemplate = ({
               paragraph
               className={classes.enText}
             >
-              (English)
-              Generativemasksはジェネラティブアートにより生成されるNFTアートです。
-              本NFTは一つずつ異なる輪郭のマスクが生成され、NFTマーケットプレイス上でリロードするたびに異なる色彩のマスクをお楽しみいただくことができます。
+              Generativemasks is an NFT art created by Generative Art. This NFT
+              "generates" unique masks with different expressions one by one,
+              and you can enjoy different colors of the masks every time you
+              reload them on the NFT marketplace.
             </Typography>
             <Typography
               variant="body1"
